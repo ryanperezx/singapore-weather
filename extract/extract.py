d0d310e55e15afcff22c36a9785679b4b67d3554
ryanperezx <ryanperez473@gmail.com>
2023-08-16 10:48:06 +0800

    feat: directories for different execution period, reworked put object process

--- extract/extract.py
+++ extract/extract.py
@@ -17,7 +17,7 @@ def get_request_content(link, headers) -> str:
     return request
 
 
-def lambda_handler(event=None, context=None):
+def lambda_handler(event: dict, context):
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -27,18 +27,21 @@ def lambda_handler(event=None, context=None):
         'Accept-Language': 'en-US,en;q=0.8',
         'Connection': 'keep-alive'
     }
+    if event is not None and event['execution_datetime']:
+        link = f'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time={event["execution_datetime"]}'
+    else:
+        link = 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
 
-    link = 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
     s3_bucket = os.environ.get('BUCKET_NAME')
 
     data = get_request_content(link, headers)
-    s3 = boto3.resource('s3')
-    s3_object = s3.Object(s3_bucket, 'data.json')
-    s3_object.put(
-        Body=(bytes(json.dumps(data.json()).encode('UTF-8')))
+    data['created_at'] = event['execution_datetime']
+
+    s3 = boto3.client('s3')
+    response = s3.put_object(
+        Body=(bytes(json.dumps(data.json()).encode('UTF-8'))),
+        Bucket=s3_bucket,
+        Key=f'singapore_weather/{event["execution_datetime"]}.json'
     )
     
-    return 200
-
-if __name__ == '__main__':
-    lambda_handler()
+    return response
