4c759572e0e9bc1bebd42e57c0ef0c9b65a6439b
ryanperezx <ryanperez473@gmail.com>
2023-08-17 09:17:41 +0800

    feat: file upload exception handling

--- extract/extract.py
+++ extract/extract.py
@@ -4,6 +4,8 @@ import json
 import boto3    
 import os
 import pytz
+from datetime import datetime
+import botocore
 
 logger = logging.getLogger(__name__)
 if logging.getLogger().hasHandlers():
@@ -19,7 +21,7 @@ def get_request_content(link, headers) -> requests.Response:
 
 
 def lambda_handler(event: dict, context) -> requests.Response:
-
+    ret_val = 200
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -33,20 +35,27 @@ def lambda_handler(event: dict, context) -> requests.Response:
     else:
         link = 'https://api.data.gov.sg/v1/environment/2-hour-weather-forecast'
 
-    s3_bucket = os.environ.get('BUCKET_NAME')
+    s3_bucket_name = os.environ.get('BUCKET_NAME')
 
     data = get_request_content(link, headers)
     data = data.json()
     if event is not None and 'execution_datetime' in event.keys():
         data['created_at'] = event['execution_datetime']
     else:
-        data['created_at'] = pendulum.now('Asia/Manila').strftime('%Y-%m-%dT%H:%M:%S')
+        data['created_at'] = datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%dT%H:%M:%S')
 
-    s3_bucket = boto3.resource('s3').Bucket(s3_bucket)
+    s3_bucket = boto3.resource('s3').Bucket(s3_bucket_name)
 
     response = s3_bucket.put_object(
         Body=(bytes(json.dumps(data).encode('utf-8'))),
         Key=f'singapore_weather/{data["created_at"]}.json'
     )
-    
-    return response
+
+    try:
+        s3_bucket.Object(s3_bucket_name, f'singapore_weather/{data["created_at"]}.json').load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            ret_val = 404
+        else:
+            raise e
+    return ret_val
