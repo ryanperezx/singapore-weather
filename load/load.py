83969828634c8c0977f3cc3f38aab86e25ae84b8
ryanperezx <ryanperez473@gmail.com>
2023-08-22 14:09:39 +0800

    fix: s3 object to python dict

--- load/load.py
+++ load/load.py
@@ -3,6 +3,8 @@ import logging
 import json
 import boto3    
 import os
+import pytz
+import datetime
 
 logger = logging.getLogger(__name__)
 if logging.getLogger().hasHandlers():
@@ -15,11 +17,15 @@ else:
 def lambda_handler(event: dict, context) -> requests.Response:
     s3_bucket_name = os.environ.get('BUCKET_NAME')
     dynamodb_table_name = os.environ.get('DYNAMODB_TABLE')
-    s3_file_path = f'singapore_weather/{event["execution_datetime"]}.json'
+    if event is not None and 'execution_datetime' in event.keys():
+        s3_file_path = f'singapore_weather/{event["execution_datetime"]}.json'
+    else:
+        s3_file_path = datetime.now(pytz.timezone('Asia/Manila')).strftime('%Y-%m-%dT%H:%M:%S')
+    
     s3_client = boto3.client('s3')
     dynamodb_client = boto3.client('dynamodb')
     
-    json_object = json.loads(s3_client.get_object(Bucket=s3_bucket_name, Key=s3_file_path))
+    json_object = json.loads(s3_client.get_object(Bucket=s3_bucket_name, Key=s3_file_path)['Body'].read())
 
     response = dynamodb_client.put_item(TableName=dynamodb_table_name, Item=json_object)
 
